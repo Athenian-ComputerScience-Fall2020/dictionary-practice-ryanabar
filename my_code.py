869ca4814d35ed66ac56a9bb8faf50b0a72f8789
ryanabar <21rabar@athenian.org>
2020-10-21 18:17:21 +0000

    Hmwk Submission

--- my_code.py
+++ my_code.py
@@ -7,22 +7,22 @@ def make_dict():
     keys = ['Ten', 'Twenty', 'Thirty']
     values = [10, 20, 30]
     '''
-    currency =          # complete this line
+    currency = {'Ten': 10, 'Twenty': 20, 'Thirty': 30}         # complete this line
 
     return currency
 
 def add_element():
     foods = {'fruit': 'apple', 'veggie': 'carrot', 'grain': 'barley'}
     # add a key value pair 'dairy': 'yogurt' to the following dictionary
-
+    
     # add code here
-
+    foods['dairy'] = 'yogurt'
     return foods
 
 def remove_element():
     foods = {'fruit': 'apple', 'veggie': 'carrot', 'grain': 'barley'}
     # remove 'veggie': 'carrot' from the dictionary
-
+    del foods['veggie']
 
     return foods
 
@@ -32,13 +32,15 @@ def merge_dict():
     dict2 = {'Thirty': 30, 'Fourty': 40, 'Fifty': 50}
     # add code here
 
-    return     # return new dictionary
+    dict3 = dict1 + dict2
+    return dict3    # return new dictionary
 
 def access_key():
     # return the value of the key 'Twenty'
     currency = {'Ten': 10, 'Twenty': 20, 'Thirty': 30}
 
-    val =             # add code to assign the desired value to 'val'
+    val = currency.values('Twenty')
+    # add code to assign the desired value to 'val'
     return val
 
 
@@ -48,5 +50,5 @@ def access_key():
 if __name__ == '__main__':
     # Test your code with this first
     # Change the function to test different sections
-    print(make_dict())
+    print(merge_dict())
 
