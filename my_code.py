da413f72c96785ba018ac898551edbb7c0bc3b29
ryanabar <21rabar@athenian.org>
2020-10-21 18:56:18 +0000

    FINAL

--- my_code.py
+++ my_code.py
@@ -31,15 +31,15 @@ def merge_dict():
     dict1 = {'Ten': 10, 'Twenty': 20, 'Thirty': 30}
     dict2 = {'Thirty': 30, 'Fourty': 40, 'Fifty': 50}
     # add code here
+    dict1.update(dict2)
 
-    dict3 = dict1 + dict2
-    return dict3    # return new dictionary
+    return dict1  # return new dictionary
 
 def access_key():
     # return the value of the key 'Twenty'
     currency = {'Ten': 10, 'Twenty': 20, 'Thirty': 30}
 
-    val = currency.values('Twenty')
+    val = currency['Twenty']
     # add code to assign the desired value to 'val'
     return val
 
